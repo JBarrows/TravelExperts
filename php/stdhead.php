a996cf0721113a77e54591e227f9c2aa5037697c
Joel Barr <Joelbarr1220@gmail.com>
2018-05-31 08:37:05 -0600

    changed the referenced folder of the favicon

--- php/stdhead.php
+++ php/stdhead.php
@@ -1,5 +1,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<link rel="icon" href="favicon.png" />
+<link rel="icon" href="img/favicon.png" />
 <link type="text/css" rel="stylesheet" href="style.css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Poppins" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/solid.css" integrity="sha384-Rw5qeepMFvJVEZdSo1nDQD5B6wX0m7c5Z/pLNvjkB14W6Yki1hKbSEQaX9ffUbWe" crossorigin="anonymous">
