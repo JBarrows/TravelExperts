292fb2844b6ab62d8b49f021ba65dd62d80ad37b
Olaoluwa Adesanya <790232@ACDM.DS.SAIT.CA>
2018-06-06 21:03:25 -0600

    90% done with my php
    added a logout functionalkty to the nav and created the session

--- myaccount.php
+++ myaccount.php
@@ -3,6 +3,61 @@
     for Travel Experts Threaded Project
     PROJ-207-B  OOSD Spring 2018, SAIT
 ------------------------------------------------->
+<?php
+	session_start();
+    // Get login session information
+    if (isset($_SESSION['userid'])) {
+        $user = $_SESSION['userid'];
+    } else {
+        // Default to Gerard Biers #143
+        $user = 143;
+
+        // TODO: Redirect to registration page instead
+        // header("location: login.php?error=invalid+access");
+    }
+
+    // Check if a new package has been ordered
+    if (isset($_SESSION['orderpackageid']))
+    {
+        $packageid = $_SESSION['orderpackageid'];
+        // SELECT * FROM `packages_products_suppliers` WHERE `PackageId`=$packageid
+        // while ($prodSupId = FETCH_ASSOC(^^))
+            // SELECT * FROM `products_suppliers` WHERE `ProductSupplierId`=$prodSupId;
+            // $prodid = FETCH_ASSOC(^^)['ProductId']
+            // SELECT * FROM `products` WHERE `ProductId`=$prodid;
+    }
+
+    $servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "travelexperts";
+
+	try {
+        /* Connect to Database */
+		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+		//create a logfile to track errors in database insert
+		$logfile = fopen("logfile.txt", "w");
+		fwrite($logfile, "Connection Successful.. Reading data for CustomerId=$user...\n");
+
+		// set the PDO error mode to exception so it throws an exception when there is an error
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        /* Fetch Customer Information */
+        $attributes = "`CustomerId`, `CustFirstName`, `CustLastName`, `CustAddress`, `CustCity`, `CustProv`, `CustPostal`, `CustCountry`, `CustHomePhone`, `CustBusPhone`, `CustEmail`";
+        $result = $conn->query("SELECT $attributes FROM `customers` WHERE `CustomerId`=$user;");
+        $custInfo = $result->fetch(PDO::FETCH_ASSOC);
+
+        /* Fetch booking information */
+        $bookingStmt = $conn->query("SELECT * FROM `bookings` WHERE `CustomerId`=$user;");
+
+    } catch(PDOException $e) {
+		$errorString = $e->getMessage();
+		fwrite($logfile, "Error!: " .$errorString);
+        echo $errorString;
+	}
+?>
+
 <!doctype="html">
 <html>
 <head>
@@ -28,17 +83,19 @@
                     <i class="fas fa-pencil-alt"></i>
                 </button>
                 <b>Name: </b>
-                Joel Barr
+                <?php echo $custInfo['CustFirstName'] . ' ' . $custInfo['CustLastName']; ?>
             </div>
             <form class='collapse' data-parent='#accordion' id='nameform' name='nameform' action="">
                 <div class='form-row'>
                     <div class="form-group col-sm-4 col-md-3">
                         <label for='fname'>First Name</label>
-                        <input type='text' class='form-control' id='fname' name='fname' value='Joel' />
+                        <?php
+                            echo "<input type='text' class='form-control' id='fname' name='fname' value='".$custInfo['CustFirstName']."' />";
+                        ?>
                     </div>
                     <div class="form-group col-sm-4 col-md-3">
                         <label for='lname'>Last Name</label>
-                        <input type='text' class='form-control' id='fname' name='lname' value="Barr" />
+                        <?php echo "<input type='text' class='form-control' id='fname' name='lname' value='".$custInfo['CustLastName']."' />"; ?>
                     </div>
                 </div>
                 <div class='form-group'>
@@ -50,18 +107,20 @@
                     <i class="fas fa-pencil-alt"></i>
                 </button>
                 <strong>Phone: </strong>
-                403-555-5520 (Business)<br>
-                403-555-4555 (Home)
+                <?php
+                    echo $custInfo['CustHomePhone'] . ' (Home)<br>' .
+                        $custInfo['CustBusPhone'] . ' (Business)';
+                ?>
             </div>
             <form class='collapse' data-parent='#accordion' id='phoneform' name='phoneform'>
                 <div class='form-row'>
                     <div class='form-group col-sm-4 col-md-3'>
-                        <label for='bphone'>Business</label>
-                        <input type='text' class='form-control' id='bphone' name='bphone' value='403-555-5520' />
+                        <label for='hphone'>Home</label>
+                        <?php echo "<input type='text' class='form-control' id='hphone' name='hphone' value='".$custInfo['CustHomePhone']."' />"; ?>
                     </div>
                     <div class=' form-group col-sm-4 col-md-3'>
-                        <label for='hphone'>Home</label>
-                        <input type='text' class='form-control' id='bphone' name='hphone' value='403-555-4555' />
+                        <label for='bphone'>Business</label>
+                        <?php echo "<input type='text' class='form-control' id='bphone' name='bphone' value='".$custInfo['CustBusPhone']."' />"; ?>
                     </div>
                 </div>
                 <div class='form-group'>
@@ -73,31 +132,33 @@
                     <i class="fas fa-pencil-alt"></i>
                 </button>
                 <strong>Address:</strong>
-                4519 Vargas St NE<br>
-                Calgary, AB<br>
-                T3K 0M6
+                <?php
+                    echo $custInfo['CustAddress'] . '<br>' .
+                        $custInfo['CustCity'] . ', ' . $custInfo['CustProv'] . '<br>' .
+                        $custInfo['CustPostal'];
+                ?>
             </div>
             <form class='collapse' data-parent='#accordion' id='addrform' name='addrform'>
                 <div class='form-row'>
                     <div class='form-group col-sm-8 col-md-6'>
                         <label for='streetaddr'>Street</label>
-                        <input type='text' class='form-control' id='streetaddr' name='streetaddr' value='4519 Vargas St NE'/>
+                        <?php echo "<input type='text' class='form-control' id='streetaddr' name='streetaddr' value='".$custInfo['CustAddress']."'/>"; ?>
                     </div>
                 </div>
                 <div class='form-row'>
                     <div class='form-group col-sm-4 col-md-3'>
                         <label for='city'>City</label>
-                        <input type='text' class='form-control' id='city' name='city' value='Calgary'/>
+                        <?php echo "<input type='text' class='form-control' id='city' name='city' value='".$custInfo['CustCity']."'/>"; ?>
                     </div>
                     <div class='form-group col-sm-4 col-md-3'>
                         <label for='prov'>Province</label>
-                        <input type='text' class='form-control' id='prov' name='prov' value='AB'/>
+                        <?php echo "<input type='text' class='form-control' id='prov' name='prov' value='".$custInfo['CustProv']."'/>"; ?>
                     </div>
                 </div>
                 <div class='form-row'>
                     <div class='form-group col-sm-4 col-md-3'>
                         <label for='postcode'>Postal Code</label>
-                        <input type='text' class='form-control' id='postcode' name='postcode' value='T3K 0M6' />
+                        <?php echo "<input type='text' class='form-control' id='postcode' name='postcode' value='".$custInfo['CustPostal']."' />"; ?>
                     </div>
                 </div>
                 <div class='form-group'>
@@ -110,33 +171,54 @@
             My Orders
         ------------------------->
         <h2>My Orders</h2>
-        <div class='row'>
-            <p class='col-3 col-md-2 col-xl-2'>Booking ID</p>
-            <p class='col-9 col-md-10 col-lg-7'>Information</p>
-        </div>
-        <div class='row bg-2'>
-            <div class='col-3 col-md-2 col-lg-2'>35653B</div>
-            <div class='col-9 col-md-10 col-lg-7'>
+        <?php
+            if ($bookingStmt == false) {
+                echo "<p>(No orders Found)</p>";
+            }
+            echo <<<EOT
                 <div class='row'>
-                    <p class='col-md-4 col-lg-3'><strong>London, England</strong></p>
-                    <p class='col-md-8 col-lg-9'>2016-05-09 until 2016-06-03</p>
+                    <p class='col-3 col-md-2 col-xl-2'>Booking ID</p>
+                    <p class='col-9 col-md-10 col-lg-7'>Information</p>
                 </div>
-                <div class='row'>
-                    <p class='col'>All-inclusive European tour</p>
-                </div>
-                <div class='row'>
-                    <div class='col-4 col-lg-3'>
-                        <b>Date Placed</b><br>2000-05-04
+EOT;
+            $nthRow = "";
+            while ($booking = $bookingStmt->fetch(PDO::FETCH_ASSOC)) {
+                $detailStmt = $conn->query("SELECT * FROM `bookingdetails` WHERE `BookingId`={$booking['BookingId']};");
+                $nthRow = ($nthRow == "") ? "bg-2" : ""; //Set the background color for odd numbered rows
+                echo <<<OPENBOOKING
+                    <div class='row {$nthRow}'>
+                        <div class='text-right col-3 col-md-2 col-lg-2'>{$booking['BookingNo']}</div>
+                        <div class='col-9 col-md-10 col-lg-7'>
+OPENBOOKING;
+                while ($details = $detailStmt->fetch(PDO::FETCH_ASSOC)) {
+                    echo <<<TRIPDETAILS
+                                <div class='row'>
+                                <p class='col-md-4 col-lg-3'><strong>{$details['Destination']}</strong></p>
+                                <p class='col-md-8 col-lg-9'>{$details['TripStart']} until {$details['TripEnd']}</p>
+                            </div>
+                            <div class='row'>
+                                <p class='col'>{$details['Description']}</p>
+                            </div>
+TRIPDETAILS;
+                }
+
+                echo <<<CLOSEBOOKING
+                            <div class='row'>
+                                <div class='col-4 col-lg-3'>
+                                    <b>Date Placed</b><br>{$booking['BookingDate']}
+                                </div>
+                                <div class='col-4 col-lg-3'>
+                                    <b>Travelers</b><br>{$booking['TravelerCount']}
+                                </div>
+                                <div class='col-4 col-lg-3'>
+                                    <b>Type</b><br>{$booking['TripTypeId']}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class='col-4 col-lg-3'>
-                        <b>Travelers</b><br>1
-                    </div>
-                    <div class='col-4 col-lg-3'>
-                        <b>Type</b><br>Leisure
-                    </div>
-                </div>
-            </div>
-        </div>
+CLOSEBOOKING;
+            }
+        ?>
 
         <!-- TODO: Sign in button can be removed once php is active -->
         <button onclick="window.location.href = 'signin.html'">Sign In</button>
