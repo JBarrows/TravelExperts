3b4701ebdcd9570a3f9a5a553b276a3bb582267b
Joel Barr <Joelbarr1220@gmail.com>
2018-06-07 14:07:01 -0600

    Enhanced Header and Nav

--- php/nav.php
+++ php/nav.php
@@ -8,27 +8,28 @@
             <a class='nav-item nav-link' href="index.php"><i class="fas fa-home"></i></a>
             <a class='nav-item nav-link' href="packages.php">Packages</a>
             <a class='nav-item nav-link' href="contact.php">Contact</a>
-            <div class='nav-item dropdown'>
-                <?php
-                if (session_status() == PHP_SESSION_NONE) { session_start(); }
-                // Get login session information
-                if (isset($_SESSION['userid'])) {
-                    echo <<<DROPDOWN
-                <a class='nav-link dropdown-toggle cursor-pointer' role='button' data-toggle='dropdown'>
-                    My Account
-                </a>
+            <?php
+            if (session_status() == PHP_SESSION_NONE) { session_start(); }
+            // Get login session information
+            if (isset($_SESSION['userid'])) {
+                echo <<<DROPDOWN
+                <div class='nav-item dropdown'>
+                    <a class='nav-link dropdown-toggle cursor-pointer' role='button' data-toggle='dropdown'>
+                        My Account
+                    </a>
 
-                <div class='dropdown-menu dropdown-menu-right'>
-                    <a class='dropdown-item' href='php/logout.php'>Sign Out</a>
-                    <a class='dropdown-item' href='myaccount.php'>My Account</a>
+                    <div class='dropdown-menu dropdown-menu-right'>
+                        <a class='dropdown-item' href='php/logout.php'>Sign Out</a>
+                        <a class='dropdown-item' href='myaccount.php'>My Account</a>
+                    </div>
                 </div>
 DROPDOWN;
-                } else {
-                    echo "<a class='nav-link cursor-pointer' role='button' href='register.php'>
-                        Sign In
-                    </a>";
-                }
-                ?>
+            } else {
+                echo "<a class='nav-link cursor-pointer' role='button' href='register.php'>
+                    Sign In
+                </a>";
+            }
+            ?>
         </div>
     </div>
 </nav>
